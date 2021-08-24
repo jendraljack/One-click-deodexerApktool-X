86a4503e4a4308ec26e8a30594df5d2a321277bb
Limitb.reaker <supriyadi185@gmail.com>
2021-08-24 19:47:58 +0000

    Update Makefolderapp.sh

--- Makefolderapp.sh
+++ Makefolderapp.sh
@@ -1,17 +1,12 @@
- #!/system/bin/sh
-
+#!/system/bin/sh
 makefolder="$(dirname $0)"
-
 ######
-
 find $makefolder -name "*.apk" > $makefolder/list.sh
-
 busybox sed -i 's|.apk||g' $makefolder/list.sh
-
 cat $makefolder/list.sh|busybox awk '{print "mkdir \""$0"\""}'|sh
-
-cat $makefolder/list.sh|busybox awk '{print "cd "$0"\nmv ../\"$(basename "$0").apk\""" ./"}' > /data/local/make.sh
-
+cat $makefolder/list.sh|busybox awk '{print "cd "$0"\nmv ../\"$(basename "$0").apk\""" ./"}' > $makefolder/make.sh
+sh $makefolder/make.sh
+rm $makefolder/make.sh
 #find $makefolder -type d -exec sh $makefolder/masukanapk.sh {} \;
-
+######
 echo "Selesai" 
