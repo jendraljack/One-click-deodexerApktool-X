2bea1d99e996a533d230b555686caba1e8505e37
Limitb.reaker <supriyadi185@gmail.com>
2021-08-25 06:06:58 +0000

    Delete deodexOat.sh

--- deodexOat.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-deodexoat="$(dirname $0)"
-kds="$(getprop ro.build.version.sdk)"
-echo "$kds" > $deodexoat/mysdk_$kds
-echo "Your sdk: $kds then $deodexoat/$kds.sh will be execute."
-find $deodexoat -name "*.oat" -exec sh $deodexoat/$kds.sh {} \;
-echo "Deodex oat OK"
